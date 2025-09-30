f346169d9e1494380450b6694905860d67010769
aansell <aansell@pps.net>
2025-09-30 15:42:08 -0700

    added npc

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -13,27 +13,37 @@ class_name npc
 @export var inventory_drop : int = 0
 #@export var state
 @export var type : String = ""
-@export var target : Vector2 
+@export var target : Vector2
 
 func _ready() -> void:
+	
 	pass
 
 func _physics_process(delta: float) -> void:
 	movement(delta)
+	move_and_slide()
 	pass
 	
 
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_detection_radius_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
 
 
-func _on_area_2d_body_exited(body: Node2D) -> void:
+func _on_detection_radius_body_exited(body: Node2D) -> void:
 	pass # Replace with function body.
+
+
 
 func movement(_delta):
 	if is_hostile:
 		target = player.position
 	else:
 		target = move_points[move_point]
+	var target_direction = position.direction_to(target)
+	velocity = speed * target_direction
+	if position.distance_to(target)<10:
+		move_point+=1
+		if move_point > move_points.size()-1:
+			move_point = 0
+	
 	pass
